c00d05b53f80e0c9be84aa4b01031359d36c2499
KelemenDaniel <kelemen.daniel@student.ms.sapientia.ro>
2025-04-09 22:44:24 +0300

    Added ORM

---
+++ EscapeRoomBackend/ORM.py
@@ -0,0 +1,75 @@
+from sqlalchemy import Column, Integer, String, Text, JSON
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Story(Base):
+    __tablename__ = 'stories'
+
+    id = Column(Integer, primary_key=True)
+    description = Column(Text, nullable=False)
+
+    def __repr__(self):
+        return f"Story id={self.id} description={self.description}"
+
+    def get_description(self):
+        return self.description
+
+    def set_description(self, value):
+        self.description = value
+
+
+class Puzzle(Base):
+    __tablename__ = 'puzzles'
+
+    id = Column(Integer, primary_key=True)
+    story_id = Column(Integer, nullable=False)
+    question = Column(Text, nullable=False)
+    possible_answers = Column(JSON, nullable=False)
+    correct_answer = Column(String(255), nullable=False)
+
+    def __repr__(self):
+        return (f"Puzzle id={self.id} story_id={self.story_id} question={self.question} "
+                f"possible_answers={self.possible_answers} correct_answer={self.correct_answer}")
+
+    def get_question(self):
+        return self.question
+
+    def set_question(self, value):
+        self.question = value
+
+    def get_possible_answers(self):
+        return self.possible_answers
+
+    def set_possible_answers(self, value):
+        self.possible_answers = value
+
+    def get_correct_answer(self):
+        return self.correct_answer
+
+    def set_correct_answer(self, value):
+        self.correct_answer = value
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    password = Column(String(50), nullable=False)
+
+    def __repr__(self):
+        return f"User id={self.id} username={self.username} password={self.password}"
+
+    def get_username(self):
+        return self.username
+
+    def set_username(self, value):
+        self.username = value
+
+    def get_password(self):
+        return self.password
+
+    def set_password(self, value):
+        self.password = value
