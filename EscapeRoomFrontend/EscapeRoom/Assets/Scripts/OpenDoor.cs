bf1abe65e0559b2398ad38fd50bac8c53082c4e8
KelemenDaniel <kelemen.daniel@student.ms.sapientia.ro>
2025-06-10 22:26:01 +0300

    Finished the sixth room

---
+++ EscapeRoomFrontend/EscapeRoom/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public Animator animator;
+    bool opened = false;
+
+    public void Open()
+    {
+        if (opened) return;
+        opened = true;
+        Debug.Log("Opening door");
+        animator.SetTrigger("Open");
+    }
+
+
+}
